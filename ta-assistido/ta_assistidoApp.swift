d6e2094e2198cfbaf220de1051f53f06df74546b
Yuri Cunha <yurastico@Yuris-Mac-mini.local>
2023-10-11 19:58:03 -0300

    first commit

---
+++ ta-assistido/ta_assistidoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ta_assistidoApp.swift
+//  ta-assistido
+//
+//  Created by Yuri Cunha on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ta_assistidoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
